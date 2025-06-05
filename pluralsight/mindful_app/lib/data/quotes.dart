5b68f3c19eee9484c5cbf544da8ebe825ef426a0
Rafael Savelli <rsavelli@gmail.com>
2025-06-05 16:38:44 +0100

    [flutter fundamentals] Showing the quote in the screen

---
+++ pluralsight/mindful_app/lib/data/quotes.dart
@@ -0,0 +1,11 @@
+class Quote {
+  final String text;
+  final String author;
+
+  Quote({required this.text, required this.author});
+
+  Quote.fromJSON(Map<String, dynamic> map)
+    : text = map['q'] ?? '',
+      author = map['a'] ?? '';
+
+}
--- pluralsight/mindful_app/lib/screens/quote_screen.dart
+++ pluralsight/mindful_app/lib/screens/quote_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:mindful_app/data/quotes.dart';
 
 class QuoteScreen extends StatefulWidget {
   const QuoteScreen({super.key});
@@ -10,21 +13,51 @@ class QuoteScreen extends StatefulWidget {
 
 class _QuoteScreenState extends State<QuoteScreen> {
   static const address = 'https://zenquotes.io/api/random';
+  Quote quote = Quote(text: '', author: '');
 
   @override
   void initState() {
     super.initState();
-    _fetchQuote();
+    _fetchQuote().then((value) {
+      quote = value;
+      setState(() {
+        
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mindful Quote')),
+      body: Center(child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(quote.text,
+              style: const TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(quote.author,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 
   Future _fetchQuote() async {
     final Uri url = Uri.parse(address);
     final response = await http.get(url);
-    print(response.body);
+    final List quoteJson = json.decode(response.body);
+    Quote quote = Quote.fromJSON(quoteJson[0]);
+    return quote;
   }
 }
